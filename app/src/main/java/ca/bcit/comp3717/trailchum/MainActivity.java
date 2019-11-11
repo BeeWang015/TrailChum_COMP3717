b3d206238399e19380870443b0f55417296e3dd7
Benson <bensonwang015@gmail.com>
2019-11-11 15:46:12 -0800

    Project created.

---
+++ app/src/main/java/ca/bcit/comp3717/trailchum/MainActivity.java
@@ -0,0 +1,16 @@
+package ca.bcit.comp3717.trailchum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Project created.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
