08f26c9600f76cb8148748a704a71ef09726b05a
Daniel Chu <danieljanchu@outlook.com>
2019-11-13 16:38:25 -0800

    removed old code from map activity

--- app/src/main/java/ca/bcit/comp3717/trailchum/MapsActivity.java
+++ app/src/main/java/ca/bcit/comp3717/trailchum/MapsActivity.java
@@ -1,20 +1,7 @@
 package ca.bcit.comp3717.trailchum;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
-
-import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.View;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -22,21 +9,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.math.BigDecimal;
-
-import ca.bcit.comp3717.trailchum.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private LocationManager locationManager;
-    private LocationListener locationListener;
+//    private LocationManager locationManager;
+//    private LocationListener locationListener;
 
 
     @Override
@@ -64,114 +42,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        Intent intent = getIntent();
-        if (intent.getIntExtra("Place Number", 0) == 0) {
-            // Zoom into users location
-            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-            locationListener = new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-                    addMarker2Map(location);
-                }
-
-                @Override
-                public void onStatusChanged(String s, int i, Bundle bundle) {
-                }
-
-                @Override
-                public void onProviderEnabled(String s) {
-                }
-
-                @Override
-                public void onProviderDisabled(String s) {
-                }
-            };
-
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                addMarker2Map(lastKnownLocation);
-            } else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            }
-        }
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
-    private void addMarker2Map(Location location) {
-
-        String msg = String.format("Current Location: %4.3f Lat %4.3f Long.",
-                location.getLatitude(),
-                location.getLongitude());
-
-        LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
-
-        mMap.addMarker(new MarkerOptions().position(latlng).title(msg));
-
-        float zoomLevel = 6.0f;
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, zoomLevel));
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-
-                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                addMarker2Map(lastKnownLocation);
-            }
-        }
-    }
-
-    public String loadJSONFromAsset(Context context) {
-        String json = null;
-        try {
-            InputStream is = context.getAssets().open("spindr_db.json");
-
-            int size = is.available();
-
-            byte[] buffer = new byte[size];
-
-            is.read(buffer);
-
-            is.close();
-
-            json = new String(buffer, "UTF-8");
-
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        return json;
-
-    }
-
-    public void onClickSearchCC(View view) {
-
-        LatLng latlng1 = new LatLng(49.25, -123);
-        try {
-            JSONObject spindrDB = new JSONObject(loadJSONFromAsset(this));
-            JSONArray CC = spindrDB.getJSONArray("CommunityCentre");
-            for (int i = 0; i < CC.length(); i++) {
-                JSONObject eachCC = CC.getJSONObject(i);
-
-                double latitude = eachCC.getDouble("latitude");
-                double longitude = eachCC.getDouble("longitude");
-
-                LatLng latlng = new LatLng(latitude, longitude);
-                mMap.addMarker(new MarkerOptions().position(latlng).title(eachCC.getString("name")));
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        float zoomLevel = 15.0f;
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng1, zoomLevel));
-
-    }
+//    private void addMarker2Map(Location location) {
+//
+//        String msg = String.format("Current Location: %4.3f Lat %4.3f Long.",
+//                location.getLatitude(),
+//                location.getLongitude());
+//
+//        LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
+//
+//        mMap.addMarker(new MarkerOptions().position(latlng).title(msg));
+//
+//        float zoomLevel = 6.0f;
+//        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, zoomLevel));
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//
+//        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+//
+//                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//                addMarker2Map(lastKnownLocation);
+//            }
+//        }
+//    }
 
 
 }
